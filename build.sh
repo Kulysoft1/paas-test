a0b5a7265ac166c39fe27c6dcb8babf69cc3c1b0
Kuly <kuly@kuly.ro>
2026-09-14 12:14:22 +0000

    test2

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Install uv (Render's native environment doesn't have it by default)
+pip install uv
+
+# Sync dependencies
+uv sync --frozen
+
+# Run Django deployment commands
+uv run python manage.py collectstatic --no-input
+uv run python manage.py migrate
