e8711f1a13f020adf852ef3f97a36058da7374b5
Kuly <kuly@kuly.ro>
2026-09-17 12:37:19 +0000

    db in confg

--- core/settings.py
+++ core/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
+        default=os.environ.get('DATABASE_URL', f"sqlite:///{BASE_DIR / 'db.sqlite3'}"),
         conn_max_age=600,
         conn_health_checks=True,
     )
